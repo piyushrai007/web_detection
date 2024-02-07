39ba1b873a51d29c4d1dba203aee9f08934b10b4
Piyush Rai <111386563+piyushrai007@users.noreply.github.com>
2024-02-08 02:54:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
+import sys
 from ultralytics import YOLO
 from PIL import Image
 
 # Load a pretrained YOLOv8 model
 model = YOLO('best.pt') # pretrained YOLOv8n model
 
-# Run batched inference on a list of images
-results = model('signin.png',conf =0.15)  # results list
+# Check if an image path is provided through the command line
+if len(sys.argv) > 1:
+    image_path = sys.argv[1]
+else:
+    print("Please provide the path to the image as a command-line argument.")
+    sys.exit(1)
+
+# Run inference on the provided image
+results = model(image_path, conf=0.15)  # results list
 
 # Show the results
 for r in results:
     im_array = r.plot()  # plot a BGR numpy array of predictions
     im = Image.fromarray(im_array[..., ::-1])  # RGB PIL image
     im.show()  # show image
-    im.save('results.jpg') 
+    im.save('results.jpg')
